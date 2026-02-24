5b1870b535275e80048f1483bfdbbca91b8462ed
Juan_vargas <johnwilliams2050@hotmail.com>
2026-02-23 20:14:27 -0500

    Agregando el widget de la pantalla 5 y su funcionalidad

--- lib/presentation/screens/screen_5.dart
+++ lib/presentation/screens/screen_5.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import '../widget/main_button.dart';
 import 'home_screen.dart';
@@ -7,26 +8,142 @@ class Screen5 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return GuessGamePage();
+  }
+}
+
+class GuessGamePage extends StatefulWidget {
+  const GuessGamePage({super.key});
+
+  @override
+  _GuessGamePageState createState() => _GuessGamePageState();
+}
+
+class _GuessGamePageState extends State<GuessGamePage> {
+  int secretNum = Random().nextInt(101);
+  int tries = 0;
+  String message = "";
+  TextEditingController controller = TextEditingController();
+
+  void checkGuess() {
+    int? guess = int.tryParse(controller.text);
+    bool found = false;
+    if (guess == null) {
+      setState(() {
+        message = "Please enter a valid number";
+      });
+      return;
+    }
+    setState(() {
+      tries++;
+
+      if (guess < secretNum) {
+        message = "Higher!";
+      } else if (guess > secretNum) {
+        message = "Lower!";
+      } else {
+        message = "That's the number";
+        found = true;
+      }
+    });
+
+    controller.clear();
+  }
+
+  void resetGame() {
+    setState(() {
+      secretNum = Random().nextInt(101);
+      tries = 0;
+      message = "";
+      controller.clear();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Pantalla 5"), centerTitle: true),
-      body: Center(
+      backgroundColor: const Color.fromARGB(255, 181, 206, 217),
+      appBar: AppBar(
+        title: Text("Guess Game"),
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+        backgroundColor: Colors.deepPurple,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: resetGame,
+        child: Icon(Icons.refresh),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
         child: Column(
+          
+          spacing: 30,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              "Estás en la Pantalla 5",
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            Icon(
+              Icons.help_outline,
+              size: 120,
+              color: Colors.green,
             ),
-            const SizedBox(height: 20),
-            MainButton(
-              text: "Volver al Índice",
-              onPressed: () {
-                Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(builder: (context) => const HomeScreen()),
-                  (route) => false,
-                );
-              },
+            Text(
+              "Guess a number (0...100)",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepOrange,
+              ),
+            ),
+
+            TextField(
+              controller: controller,
+              enabled: message != "That's the number",
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Enter a number",
+                border: UnderlineInputBorder(),
+              ),
+            ),
+
+            ElevatedButton(
+              onPressed: checkGuess,
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(200, 50),
+                backgroundColor: Colors.lightGreenAccent,
+              ),
+              child: Text("Guess!"),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Message:",
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                message,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: message == "That's the number"
+                      ? Colors.green
+                      : Colors.black,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Tries: $tries",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.green,
+                ),
+              ),
             ),
           ],
         ),
